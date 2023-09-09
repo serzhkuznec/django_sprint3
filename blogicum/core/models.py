c2f6f99b9a3873b8f3facf7dcffc3405d6231989
Сергей Кузнецов <serzh.kuznec@bk.ru>
2023-09-09 23:33:26 +0400

    TryPep8

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class PublishedModel(models.Model):
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
-                                       help_text='Снимите галочку,'
+                                       help_text='Снимите галочку, '
                                                  'чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Добавлено')
